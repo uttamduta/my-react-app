e1cab7589734134bc71fd56180ebbbe044550dc3
Uttam Dutta <uttamdutta@hotmail.com>
2025-02-04 14:33:52 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', border: '1px solid #ccc', borderRadius: '5px', backgroundColor: '#f9f9f9' }}>
+      <h1>Hello from React!</h1>
+      <p>This is a React component embedded in Pega.</p>
+    </div>
+  );
+}
+
+export default App;
